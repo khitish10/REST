b14a0e4b7b490e74bea2fb3f3ebcac0f77ab3ae6
khitish <khitish10@gmail.com>
2022-08-28 18:11:48 +0530

    Added a new package that contains request body text for add place

--- Rest/src/Basics.java
+++ Rest/src/Basics.java
@@ -1,5 +1,8 @@
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+import files.Payload;
 
 public class Basics {
 
@@ -14,25 +17,13 @@ public class Basics {
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		
 		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}\r\n"
-				+ "")
+		.body(Payload.addPlace())//calling the request body from files->>Payload
 		.when().post("/maps/api/place/add/json")
-		.then().log().all().statusCode(200);
+		.then().log().all().statusCode(200)
+		.body("scope",equalTo("APP"))//Verify the response body has a key scope with value APP
+		.header("Server", "Apache/2.4.41 (Ubuntu)");//Check response body for Header->>Server
+		
+		//Add place->>Update place->>Validate the updated address
 		
 		
 	}
