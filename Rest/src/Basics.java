1f46c54437df07773b39889c4fc5558aa15719d8
khitish <khitish10@gmail.com>
2022-08-28 18:58:42 +0530

    Update place using PUT HTTP request

--- Rest/src/Basics.java
+++ Rest/src/Basics.java
@@ -1,4 +1,6 @@
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -8,7 +10,8 @@ public class Basics {
 
 	public static void main(String[] args) {
 		
-		//Automate Add place API
+		//Add place->>Update place->>Validate the updated address
+		
 		
 		//Given->> All inputs
 		//When->> resource,HTTP request type
@@ -16,14 +19,36 @@ public class Basics {
 		
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		
-		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		
+		//Automate Add place API
+		
+		String storeResponse = given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		.body(Payload.addPlace())//calling the request body from files->>Payload
 		.when().post("/maps/api/place/add/json")
-		.then().log().all().statusCode(200)
+		.then().assertThat().statusCode(200)//check if the sattus code is 200
 		.body("scope",equalTo("APP"))//Verify the response body has a key scope with value APP
-		.header("Server", "Apache/2.4.41 (Ubuntu)");//Check response body for Header->>Server
+		.header("Server", "Apache/2.4.41 (Ubuntu)")//Check response body for Header->>Server
+		.extract().response().asString();//extract the response body and store it in a variable
 		
-		//Add place->>Update place->>Validate the updated address
+		System.out.println(storeResponse);
+		
+		JsonPath json = new JsonPath(storeResponse);//for parsing the Json
+		String place_id = json.getString("place_id");//store the place id in a variable
+		System.out.println(place_id);
+		
+		//Update Place
+		given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
+		.body("{\r\n"
+				+ "\"place_id\":\""+place_id+"\",\r\n"//Update the place id value from above stored string
+				+ "\"address\":\"70 Street Summer walk, Mexico\",\r\n"
+				+ "\"key\":\"qaclick123\"\r\n"
+				+ "}\r\n"
+				+ "")
+		.when().put("maps/api/place/update/json")
+		.then().assertThat().log().all().statusCode(200)
+		.body("msg", equalTo("Address successfully updated"));
+		
+		
 		
 		
 	}
